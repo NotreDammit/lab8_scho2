a9ce2187eccb554fe29222fe97a3a42231e10b0c
Samuel Cho <scho2@nd.edu>
2017-03-21 10:52:08 -0400

    Lab 6 for Mobile Apps - Samuel Cho

--- java/com/cs60333/scho2/lab2_scho2/MainActivity.java
+++ java/com/cs60333/scho2/lab2_scho2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cs60333.scho2.lab2_scho2;
 
 import android.content.Intent;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
         ArrayList<String[]> temp;
         MyCsvFileReader csvFile = new MyCsvFileReader(getApplicationContext());
